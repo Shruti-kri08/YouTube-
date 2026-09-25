1a9c710f9ec94087ef959aa0a4e6e07e475fa110
Shruti-kri08 <shrutiikumarii08@gmail.com>
2026-09-25 08:08:34 +0530

    update2

--- routes/video.js
+++ routes/video.js
@@ -1,5 +1,5 @@
 const express=require('express')
-const { upload,like,dislike,getVideo,updateVideo,deleteVideo,getVideoByChannelId} = require('../controllers/videoController')
+const { upload,like,dislike,getVideo,updateVideo,deleteVideo,getVideoByChannelId, videoById} = require('../controllers/videoController')
 const router=express.Router()
 
 
@@ -11,7 +11,7 @@ router.post('/dislike/:videoId',dislike)
 
 router.get('/',getVideo)
 
-router.get('/videoById/:id',getVideoByChannelId)
+router.get('/videoById/:id',videoById)
 
 
 router.put('/update/:videoId',updateVideo)
